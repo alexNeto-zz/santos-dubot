d44ae850b070b7322df1af84b915d0ec7f51a9dd
alexNeto <alex.neto3@hotmail.com>
2018-05-14 10:24:37 -0300

    adicionado repositorio para aerodromos e suas relações

---
+++ src/main/java/com/admin/tafmetar/bancodedados/Aerodromos.java
@@ -0,0 +1,23 @@
+package com.admin.tafmetar.bancodedados;
+
+public class Aerodromos {
+
+    private String aerodromo;
+    private Aerodromos dependencia;
+
+    public String getAerodromo() {
+        return aerodromo;
+    }
+
+    public void setAerodromo(String aerodromo) {
+        this.aerodromo = aerodromo;
+    }
+
+    public Aerodromos getDependencia() {
+        return dependencia;
+    }
+
+    public void setDependencia(Aerodromos dependencia) {
+        this.dependencia = dependencia;
+    }
+}
