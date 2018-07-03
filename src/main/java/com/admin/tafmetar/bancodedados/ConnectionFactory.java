5972d12a997a44bb657af89000f4ca4ffeccb61b
alexNeto <alex.neto3@hotmail.com>
2018-07-02 22:30:42 -0300

    modificado o nome do arquivo do banco de dados

--- src/main/java/com/admin/tafmetar/bancodedados/ConnectionFactory.java
+++ src/main/java/com/admin/tafmetar/bancodedados/ConnectionFactory.java
@@ -4,14 +4,17 @@ package com.admin.tafmetar.bancodedados;
 import com.db4o.Db4oEmbedded;
 import com.db4o.ObjectContainer;
 
+import java.util.UUID;
+
 public class ConnectionFactory {
 
     public static ObjectContainer conn;
 
     public static ObjectContainer getConn() {
         if (conn == null) {
+            UUID hash = UUID.randomUUID();
             return conn = Db4oEmbedded.openFile(Db4oEmbedded
-                    .newConfiguration(), "database.lag");
+                    .newConfiguration(), hash + ".lag");
         } else {
             return conn;
         }
